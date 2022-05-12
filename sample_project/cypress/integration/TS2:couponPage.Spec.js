6d1177703e36914dad33ca937ba26587e19d91ed
guru05111984 <guru.nw10@gmail.com>
2022-05-12 21:00:46 +0530

    format

--- sample_project/cypress/integration/TS2:couponPage.Spec.js
+++ sample_project/cypress/integration/TS2:couponPage.Spec.js
@@ -1,33 +1,34 @@
 /// <reference types="cypress" />
 
-const { selectors } = require("../selectors/selectors")
+const { selectors } = require("../selectors/selectors");
 
-describe('verify the coupon page section', () => {
-    before(()=>{
-      cy.visit('/coupons/')
-    })
+describe("verify the coupon page section", () => {
+  before(() => {
+    cy.visit("/coupons/");
+  });
 
-    it('verify the page navigate to home page ',() => {
-        cy.title()
-          .should('eq', selectors.element.couponsPage)
-          
-        })  
+  it("verify the page navigate to home page ", () => {
+    cy.title().should("eq", selectors.element.couponsPage);
+  });
 
-    it(' verify the top store section should appears' ,() => {
-        cy.get("header h2").contains('Top Stores')
-        cy.get(selectors.element.topStoresTitle).first().should("be.visible")
-        cy.get(selectors.element.couponStoreItem).should('have.length',14)
-        })  
+  it(" verify the top store section should appears", () => {
+    cy.get("header h2").contains("Top Stores");
+    cy.get(selectors.element.topStoresTitle).first().should("be.visible");
+    cy.get(selectors.element.couponStoreItem).should("have.length", 14);
+  });
 
-    it('verify the Top Stores contains Url are active”',() => {
-        cy.get(selectors.element.topStoreList).then(($test)=>{
-            for ( let i =0 ; i<$test.length; i++){ 
-                const txt =  ($test[i].outerText).toLowerCase().split(' ').slice(-1).join(' ')
-                cy.get(selectors.element.topStoreIcon).eq(i).click() 
-                cy.url().should('include',txt)
-                cy.get(selectors.element.breadcrumb).first().click()
-                }
-         })
-    })
-  
-})
+  it("verify the Top Stores contains Url are active”", () => {
+    cy.get(selectors.element.topStoreList).then(($test) => {
+      for (let i = 0; i < $test.length; i++) {
+        const txt = $test[i].outerText
+          .toLowerCase()
+          .split(" ")
+          .slice(-1)
+          .join(" ");
+        cy.get(selectors.element.topStoreIcon).eq(i).click();
+        cy.url().should("include", txt);
+        cy.get(selectors.element.breadcrumb).first().click();
+      }
+    });
+  });
+});
